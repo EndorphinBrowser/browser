d662471e00a5e47bb3b7e53cdf0d12b70381b9ef
Benjamin C Meyer <ben@meyerhome.net>
2008-06-26 01:51:15 +0200

    Add support for javascript bookmarklets Issue: 53

--- src/webview.cpp
+++ src/webview.cpp
@@ -390,6 +390,15 @@ void WebView::loadFinished()
 
 void WebView::loadUrl(const QUrl &url, const QString &title)
 {
+    if (url.scheme() == QLatin1String("javascript")) {
+        QString scriptSource = url.toString().mid(11);
+        QVariant result = page()->mainFrame()->evaluateJavaScript(scriptSource);
+        if (result.canConvert(QVariant::String)) {
+            QString newHtml = result.toString();
+            setHtml(newHtml);
+        }
+        return;
+    }
     m_initialUrl = url;
     if (!title.isEmpty())
         emit titleChanged(tr("Loading..."));
