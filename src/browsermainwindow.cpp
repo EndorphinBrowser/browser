acaa4b7c68f38d5fa1cb7f3d2b334a449d68f2e4
Benjamin C Meyer <ben@meyerhome.net>
2008-07-16 00:34:45 +0200

    Minor code cleanup, add shortcut for the bookmark dialog

--- src/browsermainwindow.cpp
+++ src/browsermainwindow.cpp
@@ -74,7 +74,6 @@
 #include "tabbar.h"
 #include "tabwidget.h"
 #include "toolbarsearch.h"
-#include "ui_passworddialog.h"
 #include "webview.h"
 #include "webviewsearch.h"
 
@@ -500,6 +499,7 @@ void BrowserMainWindow::setupMenu()
     QList<QAction*> bookmarksActions;
 
     QAction *showAllBookmarksAction = new QAction(tr("Manage Bookmarks..."), this);
+    showAllBookmarksAction->setShortcut(QKeySequence(Qt::CTRL | Qt::SHIFT | Qt::Key_B));
     connect(showAllBookmarksAction, SIGNAL(triggered()), this, SLOT(slotShowBookmarksDialog()));
     m_addBookmark = new QAction(QIcon(QLatin1String(":addbookmark.png")), tr("Add Bookmark..."), this);
     m_addBookmark->setIconVisibleInMenu(false);
@@ -530,6 +530,7 @@ void BrowserMainWindow::setupMenu()
     m_enableInspector->setChecked(settings.value(QLatin1String("enableInspector"), false).toBool());
 #endif
 
+    // Help
     QMenu *helpMenu = menuBar()->addMenu(tr("&Help"));
     helpMenu->addAction(tr("About &Qt"), qApp, SLOT(aboutQt()));
     helpMenu->addAction(tr("About &Arora"), this, SLOT(slotAboutApplication()));
@@ -737,8 +738,8 @@ void BrowserMainWindow::slotFileSaveAs()
 
 void BrowserMainWindow::slotPreferences()
 {
-    SettingsDialog s(this);
-    s.exec();
+    SettingsDialog settingsDialog(this);
+    settingsDialog.exec();
 }
 
 void BrowserMainWindow::slotUpdateStatusbar(const QString &string)
