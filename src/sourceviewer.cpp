eaeba631057e0ae2c374bef64a9ac189e275434c
Christian Franke <cfchris6@ts2server.com>
2008-09-29 21:46:19 +0200

    Download Source from server, most times

--- src/sourceviewer.cpp
+++ src/sourceviewer.cpp
@@ -20,26 +20,35 @@
 #include "sourceviewer.h"
 
 #include <qlayout.h>
+#include <qmenubar.h>
+#include <qnetworkcookie.h>
+#include <qnetworkreply.h>
+#include <qnetworkrequest.h>
 #include <qplaintextedit.h>
 #include <qshortcut.h>
-#include <qmenubar.h>
+#include <qwebframe.h>
+#include <qwebpage.h>
 
+#include "browserapplication.h"
+#include "networkaccessmanager.h"
 #include "plaintexteditsearch.h"
 #include "sourcehighlighter.h"
 
-SourceViewer::SourceViewer(const QString &source, const QString &title, QWidget *parent)
+SourceViewer::SourceViewer(const QString &source,
+        const QString &title, const QUrl &url, QWidget *parent)
     : QDialog(parent)
-    , m_edit(new QPlainTextEdit(source))
+    , m_edit(new QPlainTextEdit(tr("Loading...")))
     , m_highlighter(new SourceHighlighter(m_edit->document()))
     , m_plainTextEditSearch(new PlainTextEditSearch(m_edit, this))
     , layout(new QVBoxLayout(this))
     , m_menuBar(new QMenuBar(this))
     , m_editMenu(new QMenu(tr("&Edit"), m_menuBar))
     , m_findAction(new QAction(tr("&Find"), m_editMenu))
-
 {
     setWindowTitle(QString(tr("Source of Page ")).append(title));
     resize(640, 480);
+
+    m_source = new QString(source);
 
     m_edit->setLineWrapMode(QPlainTextEdit::NoWrap);
     m_edit->setReadOnly(true);
@@ -61,5 +70,24 @@ SourceViewer::SourceViewer(const QString &source, const QString &title, QWidget 
     layout->addWidget(m_plainTextEditSearch);
     layout->addWidget(m_edit);
     setLayout(layout);
+
+    m_request = new QNetworkRequest(url);
+    m_request->setAttribute(QNetworkRequest::CacheLoadControlAttribute,
+            QNetworkRequest::PreferCache);
+    m_reply = BrowserApplication::networkAccessManager()->get(*m_request);
+    connect(m_reply, SIGNAL(finished()), this, SLOT(loadingFinished()));
+    m_reply->setParent(this);
 }
 
+void SourceViewer::loadingFinished()
+{
+    QWebPage page;
+    QByteArray response = m_reply->readAll();
+    page.mainFrame()->setContent(response,QString(),m_request->url());
+    /* If original request was POST or a different problem is there, fall
+       back to modified version of QWebFrame.toHtml() */
+    if (page.mainFrame()->toHtml() != *m_source)
+        m_edit->setPlainText(*m_source);
+    else m_edit->setPlainText(QLatin1String(response));
+    m_reply->close();
+}
