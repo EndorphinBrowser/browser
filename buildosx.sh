ee55e4127720732fa8a73938521b0b837d23ac19
Benjamin C Meyer <ben@meyerhome.net>
2008-10-07 13:25:34 -0400

    Add script to build OS X package using the new macdeployqt tool in Qt 4.5

---
+++ buildosx.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# This script requires the use of the macdeployqt tool that is found in Qt 4.5
+#
+
+APP="Arora"
+VERSION="0.4"
+BACKGROUND="src/data/512x512/arora.png"
+
+DIR="bundle"
+if [ -d $DIR.app ] ; then
+    echo "$DIR.app already exists"
+    exit 1;
+fi
+
+# build app with Qt libraries
+make distclean --quiet
+qmake -config release
+make --quiet
+$QTDIR/bin/macdeployqt $APP.app/
+
+# Create Bundle
+mkdir $DIR
+cp $BACKGROUND $DIR/.Background.png
+cp -rf $APP.app $DIR/
+hdiutil create -ov -srcfolder $DIR -format UDBZ -volname "$APP $VERSION" "$APP.dmg"
+hdiutil internet-enable -yes "$APP.dmg"
+rm -rf $DIR
+
+mv $APP.dmg $APP-intel-qt4.5.dmg
