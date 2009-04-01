8f0992fd3f7cd7953d90a3bcfccf1fbbe7452a35
Zsombor Gegesy <gzsombor@gmail.com>
2009-04-01 14:38:47 +0200

    set cookies to 'session only' if new rule introduced.

--- src/cookiejar/cookiejar.cpp
+++ src/cookiejar/cookiejar.cpp
@@ -433,8 +433,12 @@ void CookieJar::applyRules()
     QList<QNetworkCookie> cookies = allCookies();
     bool changed = false;
     for (int i = cookies.count() - 1; i >= 0; --i) {
-        if (isOnDomainList(m_exceptions_block, cookies.at(i).domain())) {
+        const QNetworkCookie &q = cookies.at(i);
+        if (isOnDomainList(m_exceptions_block, q.domain())) {
             cookies.removeAt(i);
+            changed = true;
+        } else if (isOnDomainList(m_exceptions_allowForSession, q.domain())) {
+            const_cast<QNetworkCookie&>(q).setExpirationDate(QDateTime());
             changed = true;
         }
     }
