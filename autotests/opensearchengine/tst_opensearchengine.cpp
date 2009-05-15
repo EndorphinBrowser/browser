e19abf93742de5fd4084ac6c115954312a0ae37a
Benjamin C Meyer <ben@meyerhome.net>
2009-05-14 23:37:22 -0400

    Use QAutotestgenerator and generate and implement many more tests

--- autotests/opensearchengine/tst_opensearchengine.cpp
+++ autotests/opensearchengine/tst_opensearchengine.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Jakub Wieczorek <faw217@gmail.com>
+ * Copyright 2009 Benjamin C. Meyer <ben@meyerhome.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,12 +17,15 @@
  * Boston, MA  02110-1301  USA
  */
 
-#include <QtTest/QtTest>
+#include <qtest.h>
 
-#include "opensearchengine.h"
 #include "qtest_arora.h"
 
-#include <qnetworkaccessmanager.h>
+#include <qsignalspy.h>
+#include <opensearchengine.h>
+#include <qbuffer.h>
+
+#include <qdebug.h>
 
 class tst_OpenSearchEngine : public QObject
 {
@@ -35,9 +38,59 @@ public slots:
     void cleanup();
 
 private slots:
-    void openSearchEngine();
-    void imageLoading();
+    void opensearchengine_data();
+    void opensearchengine();
+
+    void description_data();
+    void description();
+    void image();
+    void imageUrl_data();
+    void imageUrl();
+    void isValid_data();
+    void isValid();
+    void name_data();
+    void name();
+    void operatorlessthan();
+    void operatorequal_data();
+    void operatorequal();
+    void providesSuggestions_data();
+    void providesSuggestions();
+    void requestSuggestions_data();
     void requestSuggestions();
+    void searchParameters_data();
+    void searchParameters();
+    void searchUrl_data();
+    void searchUrl();
+    void searchUrlTemplate_data();
+    void searchUrlTemplate();
+    void suggestionsParameters_data();
+    void suggestionsParameters();
+    void suggestionsUrl_data();
+    void suggestionsUrl();
+    void suggestionsUrlTemplate_data();
+    void suggestionsUrlTemplate();
+    void parseTemplate_data();
+    void parseTemplate();
+};
+
+// Subclass that exposes the protected functions.
+class SubOpenSearchEngine : public OpenSearchEngine
+{
+public:
+    void call_imageChanged()
+        { return SubOpenSearchEngine::imageChanged(); }
+
+    void call_loadImage() const
+        { return SubOpenSearchEngine::loadImage(); }
+
+    QNetworkAccessManager* call_networkAccessManager() const
+        { return SubOpenSearchEngine::networkAccessManager(); }
+
+    QString call_parseTemplate(QString const& searchTerm, QString const& searchTemplate) const
+        { return SubOpenSearchEngine::parseTemplate(searchTerm, searchTemplate); }
+
+    void call_suggestions(QStringList const& suggestions)
+        { return SubOpenSearchEngine::suggestions(suggestions); }
 };
 
 // This will be called before the first test function is executed.
@@ -62,24 +115,11 @@ void tst_OpenSearchEngine::cleanup()
 {
 }
 
-// Subclass that exposes the protected functions.
-class SubOpenSearchEngine : public OpenSearchEngine
+void tst_OpenSearchEngine::opensearchengine_data()
 {
-public:
-    void call_imageChanged()
-        { return SubOpenSearchEngine::imageChanged(); }
+}
 
-    void call_loadImage()
-        { return SubOpenSearchEngine::loadImage(); }
-
-    QString call_parseTemplate(QString const &searchTerm, QString templ) const
-        { return SubOpenSearchEngine::parseTemplate(searchTerm, templ); }
-
-    void call_suggestions(QStringList const &suggestions)
-        { return SubOpenSearchEngine::suggestions(suggestions); }
-};
-
-void tst_OpenSearchEngine::openSearchEngine()
+void tst_OpenSearchEngine::opensearchengine()
 {
     SubOpenSearchEngine engine;
     QCOMPARE(engine.description(), QString());
@@ -92,8 +132,8 @@ void tst_OpenSearchEngine::openSearchEngine()
     QCOMPARE(engine.providesSuggestions(), false);
     engine.requestSuggestions(QString());
     QCOMPARE(engine.searchParameters(), QList<OpenSearchEngine::Parameter>());
-    QCOMPARE(engine.searchUrlTemplate(), QString());
     QCOMPARE(engine.searchUrl(QString()), QUrl());
+    QCOMPARE(engine.searchUrlTemplate(), QString());
     engine.setDescription(QString());
     engine.setImage(QImage());
     engine.setImageUrl(QString());
@@ -107,36 +147,420 @@ void tst_OpenSearchEngine::openSearchEngine()
     QCOMPARE(engine.suggestionsUrlTemplate(), QString());
     engine.call_imageChanged();
     engine.call_loadImage();
+    QVERIFY(engine.call_networkAccessManager());
     QCOMPARE(engine.call_parseTemplate(QString(), QString()), QString());
     engine.call_suggestions(QStringList());
 }
 
-void tst_OpenSearchEngine::imageLoading()
+void tst_OpenSearchEngine::description_data()
 {
-    OpenSearchEngine engine;
+    QTest::addColumn<QString>("description");
+    QTest::newRow("null") << QString();
+    QTest::newRow("foo") << QString("foo");
+}
 
-    QPixmap image(1, 1);
-    image.fill();
+// public QString description() const
+void tst_OpenSearchEngine::description()
+{
+    QFETCH(QString, description);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setDescription(description);
+    QCOMPARE(engine.description(), description);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+// public QImage image() const
+void tst_OpenSearchEngine::image()
+{
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
 
     QBuffer imageBuffer;
     imageBuffer.open(QBuffer::ReadWrite);
+    QPixmap image(1, 1);
+    image.fill();
     image.save(&imageBuffer, "PNG");
-
-    QSignalSpy signalSpy(&engine, SIGNAL(imageChanged()));
     engine.setImageUrl(QString("data:image/png;base64,").append(imageBuffer.buffer().toBase64()));
-    engine.image();
+    QCOMPARE(engine.image(), QImage());
 
-    QTRY_COMPARE(signalSpy.count(), 1);
+    QTRY_COMPARE(spy0.count(), 1);
+    QCOMPARE(spy1.count(), 0);
+    QVERIFY(engine.image() != QImage());
 }
 
+void tst_OpenSearchEngine::imageUrl_data()
+{
+    QTest::addColumn<QString>("imageUrl");
+    QTest::newRow("null") << QString();
+    QTest::newRow("foo") << QString("foo");
+}
+
+// public QString imageUrl() const
+void tst_OpenSearchEngine::imageUrl()
+{
+    QFETCH(QString, imageUrl);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setImageUrl(imageUrl);
+    QCOMPARE(engine.imageUrl(), imageUrl);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+void tst_OpenSearchEngine::isValid_data()
+{
+    QTest::addColumn<QString>("name");
+    QTest::addColumn<QString>("searchUrlTemplate");
+    QTest::addColumn<bool>("isValid");
+    QTest::newRow("false-0") << QString() << QString() << false;
+    QTest::newRow("false-1") << QString() << QString("x") << false;
+    QTest::newRow("false-2") << QString("x") << QString() << false;
+    QTest::newRow("true") << QString("x") << QString("y") << true;
+}
+
+// public bool isValid() const
+void tst_OpenSearchEngine::isValid()
+{
+    QFETCH(QString, name);
+    QFETCH(QString, searchUrlTemplate);
+    QFETCH(bool, isValid);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setName(name);
+    engine.setSearchUrlTemplate(searchUrlTemplate);
+    QCOMPARE(engine.isValid(), isValid);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+void tst_OpenSearchEngine::name_data()
+{
+    QTest::addColumn<QString>("name");
+    QTest::newRow("null") << QString();
+    QTest::newRow("foo") << QString("foo");
+}
+
+// public QString name() const
+void tst_OpenSearchEngine::name()
+{
+    QFETCH(QString, name);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setName(name);
+    QCOMPARE(engine.name(), name);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+// public bool operator<(OpenSearchEngine const &other) const
+void tst_OpenSearchEngine::operatorlessthan()
+{
+    SubOpenSearchEngine engine1;
+    engine1.setName("a");
+    SubOpenSearchEngine engine2;
+    engine2.setName("b");
+
+    QVERIFY(engine1 < engine2);
+}
+
+typedef QList<OpenSearchEngine::Parameter> Parameters;
+Q_DECLARE_METATYPE(Parameters)
+void tst_OpenSearchEngine::operatorequal_data()
+{
+    QTest::addColumn<QString>("name");
+    QTest::addColumn<QString>("description");
+    QTest::addColumn<QString>("imageUrl");
+    QTest::addColumn<QString>("searchUrlTemplate");
+    QTest::addColumn<QString>("suggestionsUrlTemplate");
+    QTest::addColumn<Parameters>("searchParameters");
+    QTest::addColumn<Parameters>("suggestionsParameters");
+    QTest::addColumn<bool>("operatorequal");
+    QTest::newRow("null") << QString() << QString() << QString() << QString() << QString()
+                          << Parameters() << Parameters()
+                          << true;
+    QTest::newRow("name") << QString("x") << QString() << QString() << QString() << QString()
+                          << Parameters() << Parameters()
+                          << false;
+    QTest::newRow("description") << QString() << QString("x") << QString() << QString() << QString()
+                          << Parameters() << Parameters()
+                          << false;
+    QTest::newRow("imageUrl") << QString() << QString() << QString("x") << QString() << QString()
+                          << Parameters() << Parameters()
+                          << false;
+}
+
+// public bool operator==(OpenSearchEngine const &other) const
+void tst_OpenSearchEngine::operatorequal()
+{
+    QFETCH(QString, name);
+    QFETCH(QString, description);
+    QFETCH(QString, imageUrl);
+    QFETCH(QString, searchUrlTemplate);
+    QFETCH(QString, suggestionsUrlTemplate);
+    QFETCH(Parameters, searchParameters);
+    QFETCH(Parameters, suggestionsParameters);
+    QFETCH(bool, operatorequal);
+
+    SubOpenSearchEngine engine;
+    SubOpenSearchEngine other;
+    other.setName(name);
+    other.setDescription(description);
+    other.setImageUrl(imageUrl);
+    other.setSearchUrlTemplate(searchUrlTemplate);
+    other.setSuggestionsUrlTemplate(suggestionsUrlTemplate);
+    other.setSearchParameters(searchParameters);
+    other.setSuggestionsParameters(suggestionsParameters);
+
+    QCOMPARE(engine.operator==(other), operatorequal);
+}
+
+void tst_OpenSearchEngine::providesSuggestions_data()
+{
+    QTest::addColumn<QString>("suggestionsUrlTemplate");
+    QTest::addColumn<bool>("providesSuggestions");
+    QTest::newRow("false") << QString() << false;
+    QTest::newRow("true") << QString("foo") << true;
+}
+
+// public bool providesSuggestions() const
+void tst_OpenSearchEngine::providesSuggestions()
+{
+    QFETCH(QString, suggestionsUrlTemplate);
+    QFETCH(bool, providesSuggestions);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setSuggestionsUrlTemplate(suggestionsUrlTemplate);
+    QCOMPARE(engine.providesSuggestions(), providesSuggestions);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+void tst_OpenSearchEngine::requestSuggestions_data()
+{
+    QTest::addColumn<QString>("searchTerm");
+    QTest::addColumn<QStringList>("suggestions");
+    QTest::newRow("null") << QString() << QStringList();
+    QTest::newRow("foo") << QString("foo") << (QStringList());
+    // TODO load a real engine and make sure it returns a suggestion
+}
+
+// public void requestSuggestions(QString const &searchTerm)
 void tst_OpenSearchEngine::requestSuggestions()
 {
-    OpenSearchEngine engine;
-    engine.setSuggestionsUrlTemplate("x");
-    engine.requestSuggestions("foo");
+    QFETCH(QString, searchTerm);
+    QFETCH(QStringList, suggestions);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.requestSuggestions(searchTerm);
+
+    QCOMPARE(spy0.count(), 0);
+    QTRY_COMPARE(spy1.count(), suggestions.count());
+    //QCOMPARE(spy1.value(0), suggestions);
+}
+
+void tst_OpenSearchEngine::searchParameters_data()
+{
+    QTest::addColumn<Parameters>("searchParameters");
+    QTest::newRow("null") << QList<OpenSearchEngine::Parameter>();
+    QTest::newRow("something") << (Parameters() << OpenSearchEngine::Parameter("a", "b"));
+}
+
+// public QList<OpenSearchEngine::Parameter> searchParameters() const
+void tst_OpenSearchEngine::searchParameters()
+{
+    QFETCH(Parameters, searchParameters);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setSearchParameters(searchParameters);
+    QCOMPARE(engine.searchParameters(), searchParameters);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+void tst_OpenSearchEngine::searchUrl_data()
+{
+    QTest::addColumn<QString>("searchTerm");
+    QTest::addColumn<QUrl>("searchUrl");
+    QTest::newRow("null") << QString() << QUrl();
+}
+
+// public QUrl searchUrl(QString const &searchTerm) const
+void tst_OpenSearchEngine::searchUrl()
+{
+#if 0
+    QFETCH(QString, searchTerm);
+    QFETCH(QUrl, searchUrl);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    QCOMPARE(engine.searchUrl(searchTerm), searchUrl);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+#endif
+    QSKIP("Test is not implemented.", SkipAll);
+}
+
+void tst_OpenSearchEngine::searchUrlTemplate_data()
+{
+    QTest::addColumn<QString>("searchUrlTemplate");
+    QTest::newRow("null") << QString();
+    QTest::newRow("foo") << QString("foo");
+}
+
+// public QString searchUrlTemplate() const
+void tst_OpenSearchEngine::searchUrlTemplate()
+{
+    QFETCH(QString, searchUrlTemplate);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setSearchUrlTemplate(searchUrlTemplate);
+    QCOMPARE(engine.searchUrlTemplate(), searchUrlTemplate);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+void tst_OpenSearchEngine::suggestionsParameters_data()
+{
+    QTest::addColumn<Parameters>("suggestionsParameters");
+    QTest::newRow("null") << QList<OpenSearchEngine::Parameter>();
+    QTest::newRow("something") << (Parameters() << OpenSearchEngine::Parameter("a", "b"));
+}
+
+// public QList<OpenSearchEngine::Parameter> suggestionsParameters() const
+void tst_OpenSearchEngine::suggestionsParameters()
+{
+    QFETCH(Parameters, suggestionsParameters);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setSuggestionsParameters(suggestionsParameters);
+    QCOMPARE(engine.suggestionsParameters(), suggestionsParameters);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+void tst_OpenSearchEngine::suggestionsUrl_data()
+{
+    QTest::addColumn<QString>("searchTerm");
+    QTest::addColumn<QUrl>("suggestionsUrl");
+    QTest::newRow("null") << QString() << QUrl();
+}
+
+// public QUrl suggestionsUrl(QString const &searchTerm) const
+void tst_OpenSearchEngine::suggestionsUrl()
+{
+#if 0
+    QFETCH(QString, searchTerm);
+    QFETCH(QUrl, suggestionsUrl);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    QCOMPARE(engine.suggestionsUrl(searchTerm), suggestionsUrl);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+#endif
+    QSKIP("Test is not implemented.", SkipAll);
+}
+
+void tst_OpenSearchEngine::suggestionsUrlTemplate_data()
+{
+    QTest::addColumn<QString>("suggestionsUrlTemplate");
+    QTest::newRow("null") << QString();
+    QTest::newRow("foo") << QString("foo");
+}
+
+// public QString suggestionsUrlTemplate() const
+void tst_OpenSearchEngine::suggestionsUrlTemplate()
+{
+    QFETCH(QString, suggestionsUrlTemplate);
+
+    SubOpenSearchEngine engine;
+
+    QSignalSpy spy0(&engine, SIGNAL(imageChanged()));
+    QSignalSpy spy1(&engine, SIGNAL(suggestions(QStringList const&)));
+
+    engine.setSuggestionsUrlTemplate(suggestionsUrlTemplate);
+    QCOMPARE(engine.suggestionsUrlTemplate(), suggestionsUrlTemplate);
+
+    QCOMPARE(spy0.count(), 0);
+    QCOMPARE(spy1.count(), 0);
+}
+
+void tst_OpenSearchEngine::parseTemplate_data()
+{
+    QTest::addColumn<QString>("searchTerm");
+    QTest::addColumn<QString>("searchTemplate");
+    QTest::addColumn<QString>("parseTemplate");
+    QTest::newRow("null") << QString() << QString() << QString();
+}
+
+// protected QString parseTemplate(QString const &searchTerm, QString const &searchTemplate) const
+void tst_OpenSearchEngine::parseTemplate()
+{
+    QFETCH(QString, searchTerm);
+    QFETCH(QString, searchTemplate);
+    QFETCH(QString, parseTemplate);
+
+    SubOpenSearchEngine engine;
+    QCOMPARE(engine.call_parseTemplate(searchTerm, searchTemplate), parseTemplate);
+    QSKIP("Test is not implemented.", SkipAll);
 }
 
 QTEST_MAIN(tst_OpenSearchEngine)
-
 #include "tst_opensearchengine.moc"
 
