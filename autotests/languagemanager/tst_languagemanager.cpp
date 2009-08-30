EndorphinBrowser/browser

commit 6e280ccb7323a9cf88decdcedafc933a6260774e

Enhance the language manager to fallback to the country file if the country_language file doesn't exists.

autotests/languagemanager/tst_languagemanager.cpp
@@ -43,6 +43,8 @@ private slots:
     void languagemanager_data();
     void languagemanager();
 
+    void isLanguageAvailable_data();
+    void isLanguageAvailable();
     void chooseNewLanguage_data();
     void chooseNewLanguage();
     void setCurrentLanguage_data();
@@ -111,6 +113,23 @@ void tst_LanguageManager::languagemanager()
     manager.setCurrentLanguage(QString());
 }
 
+void tst_LanguageManager::isLanguageAvailable_data()
+{
+    QTest::addColumn<QString>("language");
+    QTest::addColumn<bool>("available");
+    QTest::newRow("null") << QString() << true;
+    QTest::newRow("fallback0") << "ca_ES" << true;
+    QTest::newRow("fallback1") << "ca_ES.UTF-8" << true;
+}
+
+void tst_LanguageManager::isLanguageAvailable()
+{
+    QFETCH(QString, language);
+    QFETCH(bool, available);
+    SubLanguageManager manager;
+    QCOMPARE(manager.isLanguageAvailable(language), available);
+}
+
 void tst_LanguageManager::chooseNewLanguage_data()
 {
     QTest::addColumn<int>("foo");
@@ -149,6 +168,7 @@ void tst_LanguageManager::setCurrentLanguage_data()
     if (validLanguage.isEmpty())
         QSKIP("no languages to test with", SkipAll);
     QTest::newRow(validLanguage.toLatin1()) << validLanguage << true << validLanguage;
+    QTest::newRow("fallback") << "ca_ES" << true << "ca_ES";
 }
 
 // public void setCurrentLanguage(QString const &language)

src/languagemanager.cpp
@@ -97,13 +97,33 @@ bool LanguageManager::isLanguageAvailable(const QString &language) const
     if (language.isEmpty())
         return true;
 
+    // optimization so we don't have to load all the languages
     if (!m_loaded) {
         QString file = translationLocation() + QLatin1Char('/') + language + QLatin1String(".qm");
-        return QFile::exists(file);
+        if (QFile::exists(file))
+            return true;
     }
 
+    return (!convertStringToLanguageFile(language).isEmpty());
+}
+
+// Return an empty string if we do not have the language file for string
+QString LanguageManager::convertStringToLanguageFile(const QString &string) const
+{
+#ifdef LANGUAGEMANAGER_DEBUG
+    qDebug() << "LanguageManager::" << __FUNCTION__ << string;
+#endif
     loadAvailableLanguages();
-    return m_languages.contains(language);
+    if (m_languages.contains(string))
+       return string;
+    QLocale locale(string);
+    QString fallback = locale.name().split(QLatin1Char('_')).value(0);
+#ifdef LANGUAGEMANAGER_DEBUG
+    qDebug() << "LanguageManager::" << __FUNCTION__ << "fallback" << fallback;
+#endif
+    if (m_languages.contains(fallback)) // fallback to the country
+        return locale.name();
+    return QString();
 }
 
 bool LanguageManager::setCurrentLanguage(const QString &language)
@@ -132,10 +152,11 @@ bool LanguageManager::setCurrentLanguage(const QString &language)
 
     QTranslator *newAppTranslator = new QTranslator(this);
     QString resourceDir = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
-    bool loaded = newAppTranslator->load(m_currentLanguage, translationLocation());
+    QString languageFile = convertStringToLanguageFile(m_currentLanguage);
+    bool loaded = newAppTranslator->load(languageFile, translationLocation());
 
     QTranslator *newSysTranslator = new QTranslator(this);
-    QString translatorFileName = QLatin1String("qt_") + m_currentLanguage;
+    QString translatorFileName = QLatin1String("qt_") + languageFile;
     if (!newSysTranslator->load(translatorFileName, resourceDir)) {
         delete newSysTranslator;
         newSysTranslator = 0;
@@ -148,7 +169,7 @@ bool LanguageManager::setCurrentLanguage(const QString &language)
         return false;
     }
 
-    QLocale::setDefault(QLocale(m_currentLanguage));
+    QLocale::setDefault(QLocale(translatorFileName));
 
     // A new language event is sent to all widgets in the application
     // They need to catch it and re-translate
@@ -218,7 +239,7 @@ void LanguageManager::chooseNewLanguage()
     setCurrentLanguage(m_languages.value(selection));
 }
 
-QString LanguageManager::translationLocation() const
+QString LanguageManager::translationLocation()
 {
     QString directory = BrowserApplication::dataDirectory() + QLatin1String("/locale");
     // work without installing

src/languagemanager.h
@@ -55,7 +55,8 @@ public slots:
     void chooseNewLanguage();
 
 private:
-    QString translationLocation() const;
+    static QString translationLocation();
+    QString convertStringToLanguageFile(const QString &string) const;
     void loadAvailableLanguages() const;
 
     QString m_currentLanguage;

commit 8a0ea0d268d61dd7d143a823e5b9d774226487c9

Fix a regression introduced by 6e280ccb: set a default QLocale properly by passing a language code rather than a file path.

autotests/languagemanager/tst_languagemanager.cpp
@@ -188,6 +188,11 @@ void tst_LanguageManager::setCurrentLanguage()
     widget.retranslate = false;
     QCOMPARE(manager.setCurrentLanguage(language), success);
     QCOMPARE(manager.currentLanguage(), result);
+
+    // test if we set the default locale properly
+    // this is essential for opensearch localization
+    QCOMPARE(QLocale(manager.currentLanguage()), QLocale());
+
     qApp->processEvents();
     QCOMPARE(widget.retranslate, success);
     QCOMPARE(spy.count(), success ? 1 : 0);

src/utils/languagemanager.cpp
@@ -181,7 +181,7 @@ bool LanguageManager::setCurrentLanguage(const QString &language)
         return false;
     }
 
-    QLocale::setDefault(QLocale(translatorFileName));
+    QLocale::setDefault(QLocale(m_currentLanguage));
 
     // A new language event is sent to all widgets in the application
     // They need to catch it and re-translate