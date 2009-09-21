620bb8e130b9e52499691d705ce56c8707fbdef0
Benjamin C Meyer <ben@meyerhome.net>
2009-09-20 22:50:18 -0400

    Update the copyright year

--- src/utils/singleapplication.h
+++ src/utils/singleapplication.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008, Benjamin C. Meyer
+ * Copyright (c) 2008-2009, Benjamin C. Meyer
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
