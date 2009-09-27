14c6d12513d2d2920068107193e2d45079a0a0b0
Benjamin C Meyer <ben@meyerhome.net>
2009-09-27 15:53:34 -0400

    Zsombor Gegesy agreed to put his adblockrule code under the BSD so all of adblock is now under the BSD.

--- src/adblock/adblockrule.cpp
+++ src/adblock/adblockrule.cpp
@@ -1,4 +1,5 @@
 /**
+ * Copyright (c) 2009, Zsombor Gegesy <gzsombor@gmail.com>
  * Copyright (c) 2009, Benjamin C. Meyer <ben@meyerhome.net>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -24,25 +25,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- */
-
-/*
- * Copyright (c) 2009, Zsombor Gegesy <gzsombor@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301  USA
  */
 
 #include "adblockrule.h"
