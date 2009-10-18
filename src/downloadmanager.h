17818d33aaea8759439138da7a28efb2e5c95f26
Zsombor Gegesy <gzsombor@gmail.com>
2009-10-18 14:16:53 +0200

    Show the number of downloads in the download manager dialog title, and blink it, after it's finished downloading.

--- src/downloadmanager.h
+++ src/downloadmanager.h
@@ -79,6 +79,7 @@ class DownloadItem : public QWidget, public Ui_DownloadItem
 signals:
     void statusChanged();
     void progress(qint64 bytesReceived = 0, qint64 bytesTotal = 0);
+    void downloadFinished();
 
 public:
     DownloadItem(QNetworkReply *reply = 0, bool requestFileName = false, QWidget *parent = 0);
@@ -170,12 +171,14 @@ private slots:
     void save() const;
     void updateRow(DownloadItem *item);
     void updateRow();
+    void finished();
 
 private:
     void addItem(DownloadItem *item);
     void updateItemCount();
     void load();
     bool externalDownload(const QUrl &url);
+    void updateActiveItemCount();
 
     AutoSaver *m_autoSaver;
     DownloadModel *m_model;
